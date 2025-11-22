a19e24392c698d251a2294d41509997644d1ef05
Vaishnavi Bhatale <vaishnavibhatale@gmail.com>
2025-11-22 18:22:41 +0530

    Added database SQL file

---
+++ student_result.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 22, 2025 at 01:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_result`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table student_result.admin: #1932 - Table 'student_result.admin' doesn't exist in engine
+-- Error reading data for table student_result.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `student_result`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+-- Error reading structure for table student_result.department: #1932 - Table 'student_result.department' doesn't exist in engine
+-- Error reading data for table student_result.department: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `student_result`.`department`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+-- Error reading structure for table student_result.result: #1932 - Table 'student_result.result' doesn't exist in engine
+-- Error reading data for table student_result.result: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `student_result`.`result`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+-- Error reading structure for table student_result.student: #1932 - Table 'student_result.student' doesn't exist in engine
+-- Error reading data for table student_result.student: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `student_result`.`student`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+-- Error reading structure for table student_result.subject: #1932 - Table 'student_result.subject' doesn't exist in engine
+-- Error reading data for table student_result.subject: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `student_result`.`subject`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+-- Error reading structure for table student_result.teacher: #1932 - Table 'student_result.teacher' doesn't exist in engine
+-- Error reading data for table student_result.teacher: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `student_result`.`teacher`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
